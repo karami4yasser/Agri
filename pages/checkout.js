7cdd27c5dfa87724aef17456c765429cf65aa1fb
karami4yasser <83478271+karami4yasser@users.noreply.github.com>
2022-01-23 05:43:52 +0100

    Add files via upload

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,36 +1,12 @@
-import React from "react";
-import Subtotal from "../components/Subtotal";
-import { useStateValues } from "../components/StateProvider";
-import CheckoutProduct from "../components/CheckoutProduct";
-function Checkout() {
-  const [{ user, basket }, dispatch] = useStateValues();
+import Header from "../components/Header";
+import Checkout from "../components/Checkout";
+function checkout() {
+  window.scrollTo(0, document.querySelector("div").scrollHeight);
 
   return (
-    <div
-      className="flex flex-col md:flex-row md:items-start   p-4 b h-max
-     items-center"
-    >
-      <div className="checkout_left">
-        <div>
-          <h3>hello ,{user?.email}</h3>
-          <h2 className="mr-">Your Shoping Basket</h2>
-
-          {basket.map((item) => (
-            <CheckoutProduct
-              id={item.id}
-              title={item.title}
-              image={item.image}
-              price={item.price}
-              rating={item.rating}
-            />
-          ))}
-        </div>
-      </div>
-      <div className="checkout_right">
-        <Subtotal />
-      </div>
+    <div>
+      <Header /> <Checkout />
     </div>
   );
 }
-
-export default Checkout;
+export default checkout;
